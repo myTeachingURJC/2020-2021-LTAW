102494d21583740b2b31575868fcfb89cd9c7709
Obijuan <obijuan.cube@gmail.com>
2021-04-18 20:01:56 +0200

    S8: Examples added

---
+++ S08/Ejemplos/Ej-05.js
@@ -0,0 +1,25 @@
+//-- Importar express
+const express = require('express');
+
+//-- Crear una aplicación web vacia
+const app = express();
+ 
+//-- Puerto donde lanzar el servidor
+const PORT = 8080;
+
+//-- Definir el punto de entrada principal de mi aplicación web
+app.get('/', function (req, res) {
+  res.send('Bienvenido a mi aplicación Web!!!');
+})
+ 
+//-- Otra vista
+app.get('/woala', (req, res) => {
+    res.send('WOALA! Chuck Norris approved!! :-)');
+    console.log("Acceso a /woala");
+});
+
+app.use(express.static('public'));
+
+//-- Lanzar el servidor
+app.listen(PORT);
+console.log("Servidor Express corriendo en puerto " + PORT);
